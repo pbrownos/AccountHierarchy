6197d7d56970932484f014c4f6e7bc6c84a500ad
pbrownos <paul.brown@outreachsolutions.us>
2025-02-25 15:32:23 -0600

    Updated LWC formatting to show all Accounts

--- .history/force-app/main/default/aura/AccountHierarchyModal/AccountHierarchyModalController_20250216110045.js
+++ .history/force-app/main/default/aura/AccountHierarchyModal/AccountHierarchyModalController_20250216110045.js
@@ -12,26 +12,11 @@
                         header: "Account Hierarchy",
                         body: content, 
                         showCloseButton: true,
-                        cssClass: "accountHierarchyModal slds-modal_large", // Changed to large and added custom class
+                        cssClass: "slds-modal_medium",
                         closeCallback: function() {
                             console.log("Modal closed");
                         }
                     });
-                    
-                    // Add a small delay to ensure the DOM is ready
-                    setTimeout(function() {
-                        // Find the modal content area and set its height
-                        var modalContent = document.querySelector('.accountHierarchyModal .slds-modal__content');
-                        if (modalContent) {
-                            // Force the LWC to take full height of the modal content
-                            var lwcContainer = modalContent.querySelector('c-account-hierarchy');
-                            if (lwcContainer) {
-                                lwcContainer.style.height = '100%';
-                                lwcContainer.style.display = 'block';
-                            }
-                        }
-                    }, 50);
-                    
                     // Close the quick action container modal so that only your overlay remains.
                     $A.get("e.force:closeQuickAction").fire();
                 } else {
